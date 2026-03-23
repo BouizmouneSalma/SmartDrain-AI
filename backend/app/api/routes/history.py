953139fde808eece6a857e7f3fd537d7eba03ba5
BouizmouneSalma <salmabouizmoun@gmail.com>
2026-03-23 19:42:08 +0100

    feat(hitory):ajout endpiont pour history

--- backend/app/api/routes/history.py
+++ backend/app/api/routes/history.py
@@ -1,1 +1,14 @@
-#route hotory
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/history", tags=["History"])
+
+fake_history = []
+
+@router.get("/")
+def get_history():
+    return fake_history
+
+@router.post("/")
+def add_history(item: dict):
+    fake_history.append(item)
+    return {"message": "Saved"}
